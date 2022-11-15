20018a5ee1b50fcc58e2d7a0ffe828ac16542491
ywc8851 <ywc8851@naver.com>
2022-11-15 10:42:54 +0900

    [feat] app.tsx에 routing 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import GlobalStyled from "./GlobalStyled";
+import Settings from "./pages/settings";
 
 function App() {
 	return (
 		<>
 			<GlobalStyled />
-			듀티풀~
+			<Routes>
+				<Route path="settings" element={<Settings />} />
+			</Routes>
 		</>
 	);
 }
