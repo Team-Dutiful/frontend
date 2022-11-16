c0b8f82f78c57fe51eb229049c865bed1fe9100f
ywc8851 <ywc8851@naver.com>
2022-11-16 19:35:31 +0900

    [feat] 설정페이지 중첩 라우팅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import Calendar from "./pages/calendar/calendar";
 import Group from "./pages/group/group";
 import Settings from "./pages/settings/settings";
 import ProfileSetting from "./pages/settings/profileSetting";
+import Setting from "./pages/settings/setting";
+import AccountSetting from "./pages/settings/accountSetting";
+import PasswordSetting from "./pages/settings/passwordSetting";
+import EmailSetting from "./pages/settings/emailSetting";
 
 const App = () => {
 	const setScreenSize = () => {
@@ -25,7 +29,12 @@ const App = () => {
 				<Route path="calendar" element={<Calendar />} />
 				<Route path="group" element={<Group />} />
 				<Route path="settings" element={<Settings />} />
-				<Route path="profile" element={<ProfileSetting />} />
+				<Route path="setting" element={<Setting />}>
+					<Route path="profile" element={<ProfileSetting />} />
+					<Route path="account" element={<AccountSetting />} />
+					<Route path="email" element={<EmailSetting />} />
+					<Route path="password" element={<PasswordSetting />} />
+				</Route>
 			</Routes>
 		</>
 	);
