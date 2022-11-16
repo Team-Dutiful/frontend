54a9bb4fa309f66fd07c5b3cbef2989a9ba344df
ywc8851 <ywc8851@naver.com>
2022-11-16 10:27:04 +0900

    [feat] 프로필 설정 페이지 markup & css & layout 구현

--- src/pages/settings/profileSetting.tsx
+++ src/pages/settings/profileSetting.tsx
@@ -1,90 +1,101 @@
 import styled from "styled-components";
-import profileImg from "../../assets/images/profileImg.png";
-import FootNavigation from "../../components/footNavigation";
+import bigProfileImg from "../../assets/images/bigProfileImg.png";
 
 const ProfileSetting = () => {
 	return (
-		<SettingsContainer>
-			<FootNavigation></FootNavigation>
-		</SettingsContainer>
+		<ProfileSettingContainer>
+			<Logo>Dutiful</Logo>
+			<ProfileSpan>프로필 설정</ProfileSpan>
+			<ProfileImgContainer>
+				<ProfileImg src={bigProfileImg}></ProfileImg>
+			</ProfileImgContainer>
+			<ProfileNameLabel htmlFor="nameInput">이름</ProfileNameLabel>
+			<ProfileNameInput type="text" id="nameInput" placeholder="이름을 입력해주세요." />
+			<NameChangeButton>변경</NameChangeButton>
+		</ProfileSettingContainer>
 	);
 };
 
 export default ProfileSetting;
 
-const SettingsContainer = styled.div`
+const ProfileSettingContainer = styled.div`
 	display: flex;
 	flex-direction: column;
+	align-items: center;
 	height: 100vh;
 	width: 360px;
 `;
 
-const Profile = styled.div`
-	width: 360px;
-	margin-bottom: 64px;
+const Logo = styled.div`
+	font-family: "Inika";
+	font-size: 2rem;
+	color: #ff8181;
+	margin-top: 78px;
+	margin-bottom: 68px;
+	text-align: center;
+`;
+
+const ProfileSpan = styled.span`
+	text-align: center;
+	font-family: "Inter";
+	font-style: normal;
+	font-weight: 700;
+	font-size: 20px;
+	line-height: 24px;
+	text-align: center;
+	color: #000000;
 `;
 
 const ProfileImgContainer = styled.div`
-	height: 200px;
 	display: flex;
 	justify-content: center;
-	background-color: #e86464;
 `;
 
 const ProfileImg = styled.img`
 	box-sizing: border-box;
-	width: 115.18px;
-	height: 110px;
+	width: 157.06px;
+	height: 150px;
 	border-radius: 50px;
-	margin-top: 72px;
+	margin-top: 42px;
 `;
 
-const ProfileInfo = styled.div`
-	width: 100%;
-	height: 110px;
+const ProfileNameLabel = styled.label`
+	position: absolute;
+	width: 124px;
+	height: 24px;
+	left: 12px;
+	top: 432px;
+
+	font-family: "Inter";
+	font-style: normal;
 	font-weight: 500;
-	line-height: 15px;
+	font-size: 16px;
+	line-height: 19px;
 	text-align: center;
 
+	color: #7a7a7a;
+`;
+
+const ProfileNameInput = styled.input`
+	width: 240px;
+	height: 35px;
+	margin-top: 62px;
+	margin-bottom: 216px;
+	border: 0.8px solid #a6a6a6;
+`;
+
+const NameChangeButton = styled.button`
+	width: 200px;
+	height: 50px;
+	background: #e86464;
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+	border-radius: 3px;
+	border: none;
+	font-family: "Inter";
+	font-style: normal;
+	font-weight: 600;
+	font-size: 18px;
+	line-height: 22px;
+	text-align: center;
 	color: #ffffff;
-	display: flex;
-	flex-direction: column;
-	background-color: #e86464;
-	border-radius: 0 0 50% 50%;
-`;
-
-const ProfileName = styled.span`
-	margin-top: 10px;
-	font-size: 20px;
-	margin-bottom: 10px;
-`;
-
-const ProfileEmail = styled.span`
-	font-size: 12px;
-`;
-
-const ButtonSection = styled.section`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 21px;
-
-	button {
-		border: 1px solid #e86464;
-		border-radius: 5px;
-		width: 220px;
-		height: 54px;
-		font-weight: 900;
-		font-size: 16px;
-	}
-`;
-
-const SettingButton = styled.button`
-	background: #ffffff;
-	color: #e86464;
-`;
-
-const LogoutButton = styled.button`
-	background: #ffd1d1;
-	color: #e86464;
 `;
