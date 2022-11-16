e083d1f47b76088bccedc222f593caa612c4c3c1
ywc8851 <ywc8851@naver.com>
2022-11-16 19:23:42 +0900

    [feat] 계정 설정 페이지 markup & css & layout 구현

---
+++ src/pages/settings/accountSetting.tsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import FootNavigation from "../../components/footNavigation";
+
+const AccountSetting = () => {
+	return (
+		<AccountSettingContainer>
+			<AccountSpan>계정 설정</AccountSpan>
+			<ButtonSection>
+				<EmailChangeButton>이메일 변경</EmailChangeButton>
+				<PwdChangeButton>비밀번호 변경</PwdChangeButton>
+			</ButtonSection>
+			<FootNavigation />
+		</AccountSettingContainer>
+	);
+};
+
+export default AccountSetting;
+
+const AccountSettingContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	height: 100vh;
+	width: 360px;
+`;
+
+const AccountSpan = styled.span`
+	text-align: center;
+	font-family: "Inter";
+	font-style: normal;
+	font-weight: 700;
+	font-size: 20px;
+	line-height: 24px;
+	text-align: center;
+	color: #000000;
+`;
+
+const ButtonSection = styled.section`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 21px;
+	margin-top: 70px;
+
+	button {
+		border: 1px solid #e86464;
+		border-radius: 5px;
+		width: 220px;
+		height: 54px;
+		font-weight: 900;
+		font-size: 16px;
+	}
+`;
+
+const EmailChangeButton = styled.button`
+	background: #ffffff;
+	color: #e86464;
+`;
+
+const PwdChangeButton = styled.button`
+	background: #ffd1d1;
+	color: #e86464;
+`;
